bc767269241dff16116f0b744a6b853f9c3ec6b3
saichandras <saichandrasriram06@gmail.com>
2021-02-08 19:21:17 -0600

    Added Preloader

--- js/scripts.js
+++ js/scripts.js
@@ -39,4 +39,14 @@
     $("body").scrollspy({
         target: "#sideNav",
     });
+
+    // Preloader
+    $(window).on('load', function() {
+        if ($('#preloader').length) {
+            $('#preloader').delay(100).fadeOut('slow', function() {
+                $(this).remove();
+            });
+        }
+    });
+
 })(jQuery); // End of use strict
