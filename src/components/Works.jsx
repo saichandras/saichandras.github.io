7e93b928e1546730be13632bfb4932f0143273af
saichandras <sriramsaichandra06@gmail.com>
2024-07-17 11:03:33 -0500

    Remove Card Slider and Update Deploy

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -6,7 +6,7 @@ import { SectionWrapper } from '../hoc';
 import { styles } from '../styles';
 import { getGradientClassFromString } from '../utils/colors';
 import { fadeIn, textVariant } from '../utils/motion';
-import CardSlider from './CardSlider/CardSlider';
+import CardSlider from './CardSlider';
 
 import 'swiper/css';
 import 'swiper/css/effect-cards';
