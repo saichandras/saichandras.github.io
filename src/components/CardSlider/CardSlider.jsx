fe82046e2f20a3d78c7da4fc5b55016d0a10d419
saichandras <sriramsaichandra06@gmail.com>
2024-07-16 23:54:29 -0500

    Update CardSlider and Works

--- src/components/CardSlider/CardSlider.jsx
+++ src/components/CardSlider/CardSlider.jsx
@@ -1,15 +1,21 @@
 import PropTypes from 'prop-types';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
+import './CardSlider.css';
 import { EffectCards } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 const CardSlider = ({ images }) => {
   return (
-    <Swiper effect={'cards'} grabCursor={true} modules={[EffectCards]}>
+    <Swiper
+      className="xs:w-[300px] xs:h-[648px] w-[265px] h-[573px]"
+      effect={'cards'}
+      grabCursor={true}
+      modules={[EffectCards]}
+    >
       {images.map((image, index) => (
         <SwiperSlide key={index}>
-          <img src={image} alt={`Slide ${index + 1}`} />
+          <img className="w-full h-full" src={image} alt={`Slide ${index + 1}`} />
         </SwiperSlide>
       ))}
     </Swiper>
