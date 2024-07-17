7dfa7c8d819f1f72e28131c699a08d7b1d02ca26
saichandras <sriramsaichandra06@gmail.com>
2024-07-17 11:28:14 -0500

    Another fix

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -19,7 +19,7 @@ const ProjectCard = ({ index, name, description, tags, images, source_code_link 
       viewport={{ once: true, amount: 0.01 }}
       variants={fadeIn('up', 'spring', index * 0.1, 0.55)}
     >
-      <div className="bg-tertiary p-5 rounded-2xl xs:w-[410px] md:w-[400px] w-[362px]">
+      <div className="bg-tertiary p-5 rounded-2xl md:w-[370px] xs:w-[320px] w-full">
         <div className="relative w-full h-full mb-6">
           <CardSlider images={images} />
         </div>
