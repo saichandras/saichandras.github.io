ab565b9afed7191a49757b5336e8e2017c23840a
saichandras <saichandras@users.noreply.github.com>
2024-07-18 16:37:37 +0000

    deploy: 60cc2c6da1d1b1f87002d02c7987aae2bf7515b4

---
+++ assets/index.js
@@ -0,0 +1,182 @@
+import logo from './s_logo.png';
+import backend from './backend.png';
+import creator from './creator.png';
+import mobile from './mobile.png';
+import web from './web.png';
+import github from './github.png';
+import menu from './menu.svg';
+import close from './close.svg';
+
+import algolia from './tech/algolia.png';
+import android from './tech/android.png';
+import angular from './tech/Angular.png';
+import aws from './tech/aws.png';
+import bootstrap from './tech/bootstrap.png';
+import c from './tech/c.png';
+import circleci from './tech/circleci.png';
+import css from './tech/css.png';
+import cypressjs from './tech/cypressjs.png';
+import docker from './tech/docker.png';
+import echarts from './tech/echarts.png';
+import electrode_native from './tech/electrode_native.png';
+import expressjs from './tech/expressjs.png';
+import figma from './tech/figma.png';
+import flask from './tech/flask.png';
+import fusejs from './tech/fusejs.png';
+import gcp from './tech/gcp.png';
+import git from './tech/git.png';
+import githubTech from './tech/github.png';
+import go from './tech/Go.png';
+import html from './tech/html.png';
+import html_svg from './tech/html.svg';
+import java from './tech/java.png';
+import javascript from './tech/javascript.png';
+import jetbrains from './tech/jetbrains.png';
+import jinja from './tech/jinja.png';
+import json from './tech/json.png';
+import jwt from './tech/jwt.png';
+import kotlin from './tech/kotlin.png';
+import laravel from './tech/laravel.png';
+import mongodb from './tech/mongodb.png';
+import mongoose from './tech/mongoose.png';
+import mui from './tech/mui.png';
+import mysql from './tech/mysql.png';
+import nextjs from './tech/nextjs.png';
+import nodejs from './tech/nodejs.png';
+import oracle_db from './tech/oracle-db.png';
+import php from './tech/php.png';
+import pl_sql from './tech/pl-sql.png';
+import postgresql from './tech/PostgreSQL.png';
+import preloader from './tech/Preloader.gif';
+import python from './tech/python.png';
+import rabbitmq from './tech/rabbitmq.png';
+import react from './tech/react.png';
+import reactjs from './tech/reactjs.png';
+import redis from './tech/redis.png';
+import redux from './tech/redux.png';
+import reduxSaga from './tech/reduxSaga.png';
+import saml from './tech/saml.png';
+import sql_server from './tech/sql-server.png';
+import sqla from './tech/sqla.png';
+import sso from './tech/sso.png';
+import swift from './tech/swift.png';
+import tailwind from './tech/tailwind.png';
+import terraform from './tech/terraform.png';
+import threejs from './tech/threejs.svg';
+import typescript from './tech/typescript.png';
+import vscode from './tech/vscode.png';
+import xcode from './tech/xcode.png';
+import gitlab from './tech/gitlab.png';
+
+import paradigm_logo from './company/paradigm-logo.png';
+import datachat_logo from './company/datachat_logo.png';
+import the_nine_logo from './company/the_nine_logo.png';
+import carlin_fit_llc_logo from './company/carlin_fit_llc_logo.png';
+import infosys_logo from './company/infosys_logo.png';
+
+import tracker_1 from './projects/tracker_1.gif';
+import tracker_2 from './projects/tracker_2.gif';
+import tracker_3 from './projects/tracker_3.gif';
+
+import meals_1 from './projects/meals_1.gif';
+import meals_2 from './projects/meals_2.gif';
+import meals_3 from './projects/meals_3.gif';
+
+import blogs_1 from './projects/blogs_1.gif';
+import blogs_2 from './projects/blogs_2.gif';
+import blogs_3 from './projects/blogs_3.gif';
+
+import tictactoe from './projects/TicTacToe.gif';
+import target_sum from './projects/target_sum.gif';
+import food_app from './projects/Food_App.gif';
+
+import samantha_carlin from './testimonials/samantha_carlin.jpeg';
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  algolia,
+  android,
+  angular,
+  aws,
+  bootstrap,
+  c,
+  circleci,
+  css,
+  cypressjs,
+  docker,
+  echarts,
+  electrode_native,
+  expressjs,
+  figma,
+  flask,
+  fusejs,
+  gcp,
+  git,
+  githubTech,
+  go,
+  html,
+  html_svg,
+  java,
+  javascript,
+  jetbrains,
+  jinja,
+  json,
+  jwt,
+  kotlin,
+  laravel,
+  mongodb,
+  mongoose,
+  mui,
+  mysql,
+  nextjs,
+  nodejs,
+  oracle_db,
+  php,
+  pl_sql,
+  postgresql,
+  preloader,
+  python,
+  rabbitmq,
+  react,
+  reactjs,
+  redis,
+  redux,
+  reduxSaga,
+  saml,
+  sql_server,
+  sqla,
+  gitlab,
+  sso,
+  swift,
+  tailwind,
+  terraform,
+  threejs,
+  typescript,
+  vscode,
+  xcode,
+  paradigm_logo,
+  datachat_logo,
+  the_nine_logo,
+  carlin_fit_llc_logo,
+  infosys_logo,
+  tracker_1,
+  tracker_2,
+  tracker_3,
+  meals_1,
+  meals_2,
+  meals_3,
+  blogs_1,
+  blogs_2,
+  blogs_3,
+  tictactoe,
+  target_sum,
+  food_app,
+  samantha_carlin,
+};
