77c7c1eb034e9bf391918e352f7d8edffee40355
saichandras <sriramsaichandra06@gmail.com>
2024-07-27 13:12:41 -0500

    Add Tooltip

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,6 +5,7 @@ import { technologies } from '../constants';
 import { SectionWrapper } from '../hoc';
 import { fadeIn } from '../utils/motion';
 import DynamicImageLoader from './DynamicImageLoader';
+import Tooltip from './Tooltip'; // Ensure the correct path to Tooltip component
 
 const Tech = () => {
   return (
@@ -16,19 +17,20 @@ const Tech = () => {
     >
       <div className="flex flex-row flex-wrap justify-center gap-6">
         {technologies.map((technology) => (
-          <div
-            className="sm:w-16 sm:h-16 w-14 h-14 flex items-center 
-            justify-center bg-white from-gray-100 to-gray-200 shadow-lg 
-            rounded-xl transform transition-transform hover:scale-110 hover:shadow-xl"
-            key={technology.name}
-          >
-            <DynamicImageLoader
-              unoptimized
-              src={technology.icon}
-              alt={technology.name}
-              className="sm:w-11 sm:h-11 w-10 h-10 object-contain"
-            />
-          </div>
+          <Tooltip text={technology.name} key={technology.name}>
+            <div
+              className="sm:w-16 sm:h-16 w-14 h-14 flex items-center 
+                justify-center bg-white from-gray-100 to-gray-200 shadow-lg 
+                rounded-xl transform transition-transform hover:scale-110 hover:shadow-xl"
+            >
+              <DynamicImageLoader
+                unoptimized
+                src={technology.icon}
+                alt={technology.name}
+                className="sm:w-11 sm:h-11 w-10 h-10 object-contain"
+              />
+            </div>
+          </Tooltip>
         ))}
       </div>
     </motion.div>
